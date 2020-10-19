4e8dbe787bf6b495d2e62d5a1442eb1d186e5dd2
misterkrc <misterchukwu@gmail.com>
2020-10-19 16:58:27 +0100

    Updates

---
+++ precache-manifest.896ca6b704c8386bcdb8289a9e802b77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b87732c1a18e91322e935dd144932b12",
+    "url": "/simple-react-search-field/index.html"
+  },
+  {
+    "revision": "2b7b10c4ffa2ba3a8608",
+    "url": "/simple-react-search-field/static/css/main.d120176d.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/simple-react-search-field/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-react-search-field/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b7b10c4ffa2ba3a8608",
+    "url": "/simple-react-search-field/static/js/main.15770828.chunk.js"
+  },
+  {
+    "revision": "94b705d11047d2d15dbd",
+    "url": "/simple-react-search-field/static/js/runtime-main.fc25839a.js"
+  }
+]);
